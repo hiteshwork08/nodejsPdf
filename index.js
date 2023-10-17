b8a6437a341fd4a5204e0f331c0f6b469ae0c98c
Hiteshwork@08 <Hiteshwork08@gmali.com>
2023-10-17 11:42:10 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+
+(async () => {
+  const browser = await puppeteer.launch();
+
+  const page = await browser.newPage();
+
+  const html = fs.readFileSync("sample.html", "utf-8");
+  await page.goto("data:text/html;charset=UTF-8," + html, {
+    waitUntil: "networkidle0",
+  });
+
+  await page.emulateMediaType("screen");
+
+  await page.pdf({
+    path: "result.pdf",
+    margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
+    printBackground: true,
+    format: "A4",
+  });
+
+  await browser.close();
+})();
